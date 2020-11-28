1d7cd9aeaf0bf57a2547232fec82627e82c24fce
Jasim Rashid <jasim.rashid@gmail.com>
2020-11-28 12:52:12 +0530

    replace code from lambda repo

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -17,13 +17,11 @@ def create_app():
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
     db.init_app(app)
     migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
-
     return app
 
 
